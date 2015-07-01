da7ff70d32d5f9ebbe376c5e2a8a0f867a7b3acc
kamisama <1457434343@qq.com>
2015-07-01 12:32:35 +0800

    add fire-color

---
+++ widget/color/color.js
@@ -0,0 +1,29 @@
+Editor.registerWidget( 'fire-color', {
+    is: 'fire-color',
+
+    behaviors: [EditorUI.focusable],
+
+    listeners: {
+        'focus': '_onFocus',
+        'blur': '_onBlur',
+    },
+
+    properties: {
+        color: {
+            type: Object,
+            value: function () {
+                return new Fire.color(12,33,255,0.5);
+            },
+            observer: '_colorChanged'
+        },
+    },
+
+    ready: function () {
+        this._initFocusable(this);
+    },
+
+    _colorChanged: function () {
+        this.$.previewRGB.style.backgroundColor = chroma(this.color.r, this.color.g, this.color.b, this.color.a).css('rgba');
+        this.$.alpha.style.width = (this.color.a / 1) * 100 + '%';
+    },
+});
