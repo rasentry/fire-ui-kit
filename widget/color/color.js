a7ae0bcbf7b3c93664b91c7d7b232f1b1a0e6371
kamisama <1457434343@qq.com>
2015-07-02 14:36:53 +0800

    add color-picker

--- widget/color/color.js
+++ widget/color/color.js
@@ -9,7 +9,7 @@ Editor.registerWidget( 'fire-color', {
     },
 
     properties: {
-        color: {
+        value: {
             type: Object,
             value: function () {
                 return new Fire.color(0,0,0,1);
@@ -23,12 +23,55 @@ Editor.registerWidget( 'fire-color', {
     },
 
     _colorChanged: function () {
-        this.$.previewRGB.style.backgroundColor = chroma(this.color.r, this.color.g, this.color.b, this.color.a).css('rgba');
-        this.$.alpha.style.width = (this.color.a / 1) * 100 + '%';
+        this.$.previewRGB.style.backgroundColor = chroma(this.value.r, this.value.g, this.value.b, this.value.a).css('rgba');
+        this.$.alpha.style.width = (this.value.a / 1) * 100 + '%';
     },
 
     _showColorPicker: function () {
-        var _colorPicker = document.createElement('color-picker');
-        document.body.appendChild(_colorPicker);
+        EditorUI.addHitGhost('cursor', '998', function () {
+            if (this.colorPicker) {
+                this.colorPicker.close();
+                this.colorPicker = null;
+            }
+            EditorUI.removeHitGhost();
+        }.bind(this));
+
+        this.colorPicker = document.createElement('color-picker');
+        this.colorPicker.setColor({
+            r: this.value.r,
+            g: this.value.g,
+            b: this.value.b,
+            a: this.value.a
+        });
+        this.colorPicker.addEventListener('color-changed',function (event) {
+            this.value = new Fire.color(this.colorPicker.value.r,this.colorPicker.value.g,this.colorPicker.value.b,this.colorPicker.value.a);
+        }.bind(this));
+        document.body.appendChild(this.colorPicker);
+        this.updateColorPicker();
+    },
+
+    updateColorPicker: function () {
+        window.requestAnimationFrame ( function () {
+            if ( !this.colorPicker)
+                return;
+
+            var bodyRect = document.body.getBoundingClientRect();
+            var elRect = this.getBoundingClientRect();
+            var menuRect = this.colorPicker.getBoundingClientRect();
+
+            var style = this.colorPicker.style;
+            style.position = "absolute";
+            style.right = (bodyRect.right - elRect.right) + "px";
+            style.zIndex = 999;
+
+            if ( document.body.clientHeight - elRect.bottom <= menuRect.height + 10 ) {
+                style.top = (elRect.top - bodyRect.top - menuRect.height - 5) + "px";
+            }
+            else {
+                style.top = (elRect.bottom - bodyRect.top + 5) + "px";
+            }
+
+            this.updateColorPicker();
+        }.bind(this) );
     },
 });
