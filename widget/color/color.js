a4ded12e4b700190d99ae66ceea97c56a4a55530
Johnny Wu <KarasAya@gmail.com>
2015-07-03 21:35:48 +0800

    add key handler

--- widget/color/color.js
+++ widget/color/color.js
@@ -7,6 +7,7 @@ Editor.registerWidget( 'fire-color', {
         'focus': '_onFocus',
         'blur': '_onBlur',
         'focusin': '_onFocusIn',
+        'keydown': '_onKeyDown',
     },
 
     properties: {
@@ -34,31 +35,11 @@ Editor.registerWidget( 'fire-color', {
     _onColorClick: function ( event ) {
         event.stopPropagation();
 
-        EditorUI.addHitGhost('cursor', '998', function () {
-            if (this._colorPicker) {
-                Polymer.dom(this).removeChild(this._colorPicker);
-                this._colorPicker = null;
-            }
-            EditorUI.removeHitGhost();
-        }.bind(this));
-
-        this._colorPicker = document.createElement('color-picker');
-        this._colorPicker.setColor({
-            r: this.value.r * 255|0,
-            g: this.value.g * 255|0,
-            b: this.value.b * 255|0,
-            a: this.value.a
-        });
-        this._colorPicker.addEventListener( 'value-changed', function (event) {
-            var value_ = event.target.value;
-            this.value = new Fire.Color(value_.r/255,
-                                        value_.g/255,
-                                        value_.b/255,
-                                        value_.a);
-        }.bind(this));
-
-        Polymer.dom(this).appendChild(this._colorPicker);
-        this._updateColorPicker();
+        if ( this._colorPicker ) {
+            this._closeColorPicker();
+        } else {
+            this._openColorPicker();
+        }
     },
 
     _updateColorPicker: function () {
@@ -86,7 +67,66 @@ Editor.registerWidget( 'fire-color', {
         }.bind(this) );
     },
 
+    _openColorPicker () {
+        EditorUI.addHitGhost('cursor', '998', function () {
+            this._closeColorPicker();
+        }.bind(this));
+
+        this._colorPicker = document.createElement('color-picker');
+        this._colorPicker.setColor({
+            r: this.value.r * 255|0,
+            g: this.value.g * 255|0,
+            b: this.value.b * 255|0,
+            a: this.value.a
+        });
+        this._colorPicker.addEventListener( 'value-changed', function (event) {
+            var value_ = event.target.value;
+            this.value = new Fire.Color(value_.r/255,
+                                        value_.g/255,
+                                        value_.b/255,
+                                        value_.a);
+        }.bind(this));
+
+        Polymer.dom(this).appendChild(this._colorPicker);
+        this._updateColorPicker();
+    },
+
+    _closeColorPicker () {
+        if (this._colorPicker) {
+            Polymer.dom(this).removeChild(this._colorPicker);
+            this._colorPicker = null;
+        }
+        EditorUI.removeHitGhost();
+    },
+
     _onFocusIn: function ( event ) {
         this._setFocused(true);
+    },
+
+    _onKeyDown: function ( event ) {
+        // space, enter
+        if (event.keyCode === 13 || event.keyCode === 32) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if ( this._colorPicker ) {
+                this._closeColorPicker();
+            } else {
+                this._openColorPicker();
+            }
+        }
+        // esc
+        else if (event.keyCode === 27) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if ( this._colorPicker ) {
+                this._closeColorPicker();
+            }
+            else {
+                this.setBlur();
+                EditorUI.focusParent(this);
+            }
+        }
     },
 });
