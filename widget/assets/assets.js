cf3dec2be6aa35fe02dc3acd89a040cc2db5adfb
kamisama <1457434343@qq.com>
2015-07-07 18:39:35 +0800

    add fire-assets

---
+++ widget/assets/assets.js
@@ -0,0 +1,80 @@
+Editor.registerWidget( 'fire-assets', {
+    is: 'fire-assets',
+
+    behaviors: [EditorUI.focusable],
+
+    listeners: {
+        'focus': '_onFocus',
+        'blur': '_onBlur',
+    },
+
+    properties: {
+        value: {
+            type: String,
+            value: '',
+        },
+
+        type: {
+            type: String,
+            value: 'Fire.Assets'
+        },
+
+        highlighted: {
+            type: Boolean,
+            value: false,
+            reflectToAttribute: true,
+        },
+
+        invalid: {
+            type: Boolean,
+            value: false,
+            reflectToAttribute: true,
+        }
+    },
+
+    ready: function () {
+        this._initFocusable(this);
+    },
+
+    _dragOver: function (event) {
+        event.stopPropagation();
+
+        this.highlighted = true;
+        this.invalid = true;
+    },
+
+    _dragLeave: function (event) {
+        event.stopPropagation();
+
+        this.highlighted = false;
+        this.invalid = false;
+    },
+
+    _dropDone: function (event) {
+        event.stopPropagation();
+
+        this.highlighted = false;
+        this.invalid = false;
+    },
+
+    _isNullClass: function (value) {
+        if (!value) {
+            return 'null';
+        }
+        return 'text';
+    },
+
+    _isNullText: function (value) {
+        if (!value) {
+            return 'None';
+        }
+        return value;
+    },
+
+    _browseClick: function (event) {
+        event.stopPropagation();
+
+        this.fire('browse');
+    },
+
+});
