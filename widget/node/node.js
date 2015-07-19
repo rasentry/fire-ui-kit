a633f37b15e08409884a7a5f9dc319a7dde8a238
Johnny Wu <KarasAya@gmail.com>
2015-07-20 00:45:20 +0800

    add fire-node

---
+++ widget/node/node.js
@@ -0,0 +1,149 @@
+Editor.registerWidget( 'fire-node', {
+    is: 'fire-node',
+
+    behaviors: [EditorUI.focusable,EditorUI.droppable],
+
+    hostAttributes: {
+        'droppable': 'node',
+        'single-drop': true,
+    },
+
+    listeners: {
+        'focus': '_onFocus',
+        'blur': '_onBlur',
+        'click': '_onClick',
+        'drop-area-enter': '_onDropAreaEnter',
+        'drop-area-leave': '_onDropAreaLeave',
+        'drop-area-accept': '_onDropAreaAccept',
+    },
+
+    properties: {
+        value: {
+            type: String,
+            value: '',
+            notify: true,
+            observer: '_valueChanged',
+        },
+
+        type: {
+            type: String,
+            value: 'Unkown'
+        },
+
+        highlighted: {
+            type: Boolean,
+            value: false,
+            reflectToAttribute: true,
+        },
+
+        invalid: {
+            type: Boolean,
+            value: false,
+            reflectToAttribute: true,
+        }
+    },
+
+    ready: function () {
+        this._initFocusable(this);
+        this._initDroppable(this.$.dropArea);
+
+        this._nodeName = '';
+    },
+
+    _onDragOver: function (event) {
+        var dragType = EditorUI.DragDrop.type(event.dataTransfer);
+        if ( dragType !== 'node' ) {
+            EditorUI.DragDrop.allowDrop( event.dataTransfer, false );
+            return;
+        }
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        //
+        if ( this.highlighted ) {
+            if ( !this.invalid ) {
+                EditorUI.DragDrop.updateDropEffect(event.dataTransfer, 'copy');
+            }
+            else {
+                EditorUI.DragDrop.updateDropEffect(event.dataTransfer, 'none');
+            }
+        }
+        else {
+            EditorUI.DragDrop.updateDropEffect(event.dataTransfer, 'none');
+        }
+    },
+
+    _onClick: function ( event ) {
+        event.stopPropagation();
+        Editor.sendToAll('editor:hint-node', this.value);
+    },
+
+    _onDropAreaEnter: function (event) {
+        event.stopPropagation();
+
+        var dragItems = event.detail.dragItems;
+
+        // TODO
+        // Editor.assetdb.queryInfoByUuid( dragItems[0], function ( info ) {
+        //     this.highlighted = true;
+        //     if ( this.type !== info['meta-type'] ) {
+        //         this.invalid = true;
+        //     }
+        // }.bind(this));
+    },
+
+    _onDropAreaLeave: function (event) {
+        event.stopPropagation();
+
+        this.highlighted = false;
+        this.invalid = false;
+    },
+
+    _onDropAreaAccept: function (event) {
+        event.stopPropagation();
+
+        this.highlighted = false;
+        this.invalid = false;
+
+        var dragItems = event.detail.dragItems;
+        var uuid = dragItems[0];
+        this.value = uuid;
+    },
+
+    _nodeClass: function (value) {
+        if (!value) {
+            return 'null name flex-1';
+        }
+        return 'name flex-1';
+    },
+
+    _valueChanged: function () {
+        if ( !this.value ) {
+            this._nodeName = 'None';
+            return;
+        }
+
+        // TODO
+        // if ( !Editor.assetdb ) {
+        //     this._assetName = 'Unkown';
+        //     return;
+        // }
+
+        // Editor.assetdb.queryUrlByUuid( this.value, function ( url ) {
+        //     var Url = require('fire-url');
+        //     this._assetName = Url.basenameNoExt(url);
+        // }.bind(this));
+    },
+
+    _onEmptyClick: function (event) {
+        event.stopPropagation();
+
+        this.set( 'value', '' );
+    },
+
+    _onBrowseClick: function (event) {
+        event.stopPropagation();
+        Editor.info('TODO');
+    },
+});
